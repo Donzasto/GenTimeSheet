1aaf7d464eede373c3ce066c973c97e6f4f3d1df
Donzasto <fordealing@yandex.ru>
2023-09-06 08:03:27 +0300

    update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿WordValidation wordValidation = new();
+﻿using System.Diagnostics;
 
+WordValidation wordValidation = new();
+Stopwatch stopwatch = new Stopwatch();
+//засекаем время начала операции
+stopwatch.Start();
 wordValidation.CheckDaysInMonth();
 wordValidation.CheckWeekendsColor();
 wordValidation.CheckXsCount();
 wordValidation.CheckWeekendsWithEights();
 wordValidation.CheckFirstDay();
 wordValidation.CheckOrderXand8();
+stopwatch.Stop();
+//смотрим сколько миллисекунд было затрачено на выполнение
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
--- WordValidation.cs
+++ WordValidation.cs
@@ -33,12 +33,13 @@ internal class WordValidation
         _year = int.Parse(GetStringsFromParagraph(_filePath1)[^2]);
     }
 
-    string[] GetStringsFromParagraph(string filePath1) =>
-                GetFirstParagraph(filePath1).InnerText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    private static string[] GetStringsFromParagraph(string filePath1) =>
+        GetFirstParagraph(filePath1).InnerText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
     internal void CheckDaysInMonth()
     {
-        string lastDayMonth = _table1.Elements<TableRow>().ElementAt(0).Elements<TableCell>().Last().InnerText;
+        string lastDayMonth = _table1.Elements<TableRow>().First().Elements<TableCell>().Last().
+            InnerText;
 
         int daysInMonth = DateTime.DaysInMonth(_year, _month);
 
@@ -48,11 +49,10 @@ internal class WordValidation
 
     internal void CheckWeekendsColor()
     {
-        bool HasIncorrectWeekendsColor = _table1.Elements<TableRow>().ElementAt(0).Elements<TableCell>().
-                                Skip(3).
-                                Where(cells => cells.Elements<TableCellProperties>().ElementAt(0).Shading is not null &&
-                                    new DateOnly(_year, _month, int.Parse(cells.InnerText)).DayOfWeek is not (DayOfWeek.Saturday or DayOfWeek.Sunday)).
-                                Any();
+        bool HasIncorrectWeekendsColor = _table1.Elements<TableRow>().First().Elements<TableCell>().
+            Where(cells => cells.Elements<TableCellProperties>().ElementAt(0).Shading is not null &&
+            new DateOnly(_year, _month, int.Parse(cells.InnerText)).
+            DayOfWeek is not (DayOfWeek.Saturday or DayOfWeek.Sunday)).Any();
 
         if (HasIncorrectWeekendsColor)
             Console.WriteLine("weekends color");
@@ -60,29 +60,21 @@ internal class WordValidation
 
     internal void CheckXsCount()
     {
-        var schedule = new List<List<string>>();
+        var rows = _table1.Elements<TableRow>().Select(cell => cell.Elements<TableCell>().Skip(3).
+           ToList());
 
-        foreach (var row in _table1.Elements<TableRow>())
+        for (int i = 0; i < rows.First().Count; i++)
         {
-            var days = row.Elements<TableCell>().Skip(3).Select(c => c.InnerText).ToList();
-
-            schedule.Add(days);
-        }
-
-        for (int i = 0; i < schedule[0].Count; i++)
-        {
-            if (schedule.Count(r => r[i].EqualsOneOf(RU_X, EN_X)) is not (2 or 3))
+            if (rows.Count(days => days[i].InnerText.EqualsOneOf(RU_X, EN_X)) is not (2 or 3))
                 Console.WriteLine($"day {i + 1}");
         }
     }
 
     internal void CheckWeekendsWithEights()
     {
-        bool hasWeekendsWithEights = _table1.Elements<TableRow>().ElementAt(3).Elements<TableCell>().
-                                        Skip(3).
-                                        Where(cells => cells.Elements<TableCellProperties>().ElementAt(0).Shading is not null &&
-                                            cells.InnerText.EqualsOneOf(EIGHT)).
-                                        Any();
+        bool hasWeekendsWithEights = _table1.Elements<TableRow>().ElementAt(3).
+            Elements<TableCell>().Where(cells => cells.Elements<TableCellProperties>().ElementAt(0).
+            Shading is not null && cells.InnerText.EqualsOneOf(EIGHT)).Any();
 
         if (hasWeekendsWithEights)
             Console.WriteLine("weekend with eights");
@@ -90,12 +82,13 @@ internal class WordValidation
 
     internal void CheckFirstDay()
     {
-        var hasIncorrectFirstDay = _table2.Elements<TableRow>().Where(rows => rows.Elements<TableCell>().Last().InnerText.EqualsOneOf(RU_X, EN_X)).
-                                        Select(rows => rows.Elements<TableCell>().ElementAt(1).InnerText).
-                                        Intersect(_table1.Elements<TableRow>().
-                                        Where(rows => rows.Elements<TableCell>().ElementAt(3).InnerText.EqualsOneOf(RU_X, EN_X, EIGHT)).
-                                        Select(rows => rows.Elements<TableCell>().ElementAt(1).InnerText)).
-                                        Any();
+        var hasIncorrectFirstDay = _table2.Elements<TableRow>().
+            Where(rows => rows.Elements<TableCell>().Last().InnerText.EqualsOneOf(RU_X, EN_X)).
+            Select(rows => rows.Elements<TableCell>().ElementAt(1).InnerText).
+            Intersect(_table1.Elements<TableRow>().
+            Where(rows => rows.Elements<TableCell>().ElementAt(3).InnerText.
+            EqualsOneOf(RU_X, EN_X, EIGHT)).
+            Select(rows => rows.Elements<TableCell>().ElementAt(1).InnerText)).Any();
 
         if (hasIncorrectFirstDay)
             Console.WriteLine("first day");
@@ -103,27 +96,31 @@ internal class WordValidation
 
     internal void CheckOrderXand8()
     {
-        var half = _table1.Elements<TableRow>().ElementAt(3).Elements<TableCell>().Skip(3).Select(cell => cell.InnerText).ToArray();
+        var days = _table1.Elements<TableRow>().ElementAt(3).Elements<TableCell>().
+            Select(cell => cell.InnerText).ToArray();
 
-        for (int i = 0; i < half.Length - 1; i++)
+        for (int i = 3; i < days.Length - 1; i++)
         {
-            if (half[i].EqualsOneOf(RU_X, EN_X) && half[i + 1].EqualsOneOf(EIGHT))
+            if (days[i].EqualsOneOf(RU_X, EN_X) && days[i + 1].EqualsOneOf(EIGHT))
                 Console.WriteLine("X and eight");
         }
     }
 
     private static Body GetBody(string filePath)
     {
-        using WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(filePath, false);
+        using WordprocessingDocument wordprocessingDocument =
+            WordprocessingDocument.Open(filePath, false);
 
         return wordprocessingDocument.MainDocumentPart.Document.Body;
     }
 
-    private static Table GetLastTable(string filePath) => GetElements<Table>(filePath).Last();
+    internal Table GetLastTable(string filePath) => GetElements<Table>(filePath).Last();
 
-    private static Paragraph GetFirstParagraph(string filePath) => GetElements<Paragraph>(filePath).First();
+    private static Paragraph GetFirstParagraph(string filePath) =>
+        GetElements<Paragraph>(filePath).First();
 
-    private static IEnumerable<T> GetElements<T>(string filePath) where T : OpenXmlElement => GetBody(filePath).Elements<T>();
+    private static IEnumerable<T> GetElements<T>(string filePath) where T : OpenXmlElement =>
+        GetBody(filePath).Elements<T>();
 
     private static string GetFilePath(string fileName)
     {
@@ -137,5 +134,6 @@ internal class WordValidation
 
 public static class StringExtension
 {
-    public static bool EqualsOneOf(this string s, params string[] strings) => strings.Contains(s.Trim(), StringComparer.OrdinalIgnoreCase);
+    public static bool EqualsOneOf(this string s, params string[] strings) =>
+        strings.Contains(s.Trim(), StringComparer.OrdinalIgnoreCase);
 }
