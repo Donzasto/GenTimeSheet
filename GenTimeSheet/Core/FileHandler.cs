48a7394b5e97785326b85e80c0fbbd72c674122c
Donzasto <ForDealing@yandex.ru>
2024-03-30 10:28:23 +0300

    update

--- GenTimeSheet/Core/FileHandler.cs
+++ GenTimeSheet/Core/FileHandler.cs
@@ -8,7 +8,7 @@ public static class FileHandler
     public static string GetFilePath(string fileName)
     {
         string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        string file = Path.Combine(currentDirectory, @"../../../../GenTimeSheet/static/" + fileName);
+        string file = Path.Combine(currentDirectory, @"../../../../GenTimeSheet/files/" + fileName);
         string filePath = Path.GetFullPath(file);
 
         return filePath;
